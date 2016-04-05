ed9767b1ee73c2bb542afc0b77e09f06b6ca92a9
uupaa <uupaa.js@gmail.com>
2016-04-05 17:05:26 +0900

    revision up

--- test/browser/worker.js
+++ test/browser/worker.js
@@ -1,1 +1,32 @@
+// ADTS test
+
+onmessage = function(event) {
+    self.unitTest = event.data; // { message, setting: { secondary, baseDir } }
+
+    if (!self.console) { // polyfill WebWorkerConsole
+        self.console = function() {};
+        self.console.dir = function() {};
+        self.console.log = function() {};
+        self.console.warn = function() {};
+        self.console.error = function() {};
+        self.console.table = function() {};
+    }
+
+    importScripts("../../lib/WebModule.js");
+
+    WebModule.VERIFY  = true;
+    WebModule.VERBOSE = true;
+    WebModule.PUBLISH = true;
+
+    importScripts("../../node_modules/uupaa.fileloader.js/lib/FileLoader.js");
+    importScripts("../../node_modules/uupaa.hash.js/node_modules/uupaa.bit.js/lib/Bit.js");
+    importScripts("../../node_modules/uupaa.hash.js/node_modules/uupaa.bit.js/lib/BitView.js");
+    importScripts("../../node_modules/uupaa.hash.js/lib/Hash.js");
+    importScripts("../wmtools.js");
+    importScripts("../../lib/ADTS.js");
+    importScripts("../../release/ADTS.w.min.js");
+    importScripts("../testcase.js");
+
+    self.postMessage(self.unitTest);
+};
 
--- test/node/index.js
+++ test/node/index.js
@@ -1,1 +1,17 @@
+// ADTS test
+
+require("../../lib/WebModule.js");
+
+WebModule.VERIFY  = true;
+WebModule.VERBOSE = true;
+WebModule.PUBLISH = true;
+
+require("../../node_modules/uupaa.fileloader.js/lib/FileLoader.js");
+require("../../node_modules/uupaa.hash.js/node_modules/uupaa.bit.js/lib/Bit.js");
+require("../../node_modules/uupaa.hash.js/node_modules/uupaa.bit.js/lib/BitView.js");
+require("../../node_modules/uupaa.hash.js/lib/Hash.js");
+require("../wmtools.js");
+require("../../lib/ADTS.js");
+require("../../release/ADTS.n.min.js");
+require("../testcase.js");
 
