f7aadb8a16acd5ad06470bbe5203dd3429624227
uupaa <uupaa.js@gmail.com>
2016-04-05 16:58:31 +0900

    follow new WebModule version

--- test/node/index.js
+++ test/node/index.js
@@ -1,15 +1,1 @@
-// ADTS test
-
-require("../../lib/WebModule.js");
-
-WebModule.verify  = true;
-WebModule.verbose = true;
-WebModule.publish = true;
-
-require("../../node_modules/uupaa.fileloader.js/lib/FileLoader.js");
-require("../../node_modules/uupaa.hash.js/lib/Hash.js");
-require("../wmtools.js");
-require("../../lib/ADTS.js");
-require("../../release/ADTS.n.min.js");
-require("../testcase.js");
 
