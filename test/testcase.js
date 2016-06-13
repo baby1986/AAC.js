d74fef7d4691ce9aa4592e4222f887eb28ada99b
uupaa <uupaa.js@gmail.com>
2016-06-13 21:13:55 +0900

    update test

--- test/testcase.js
+++ test/testcase.js
@@ -1,14 +1,14 @@
-var ModuleTestADTS = (function(global) {
+var ModuleTestAAC = (function(global) {
 
 var test = new Test(["AAC"], { // Add the ModuleName to be tested here (if necessary).
-        disable:    false, // disable all tests.
+        disable:    true, // disable all tests.
         browser:    true,  // enable browser test.
         worker:     false,  // enable worker test.
         node:       false,  // enable node test.
         nw:         true,  // enable nw.js test.
         el:         true,  // enable electron (render process) test.
         button:     true,  // show button.
-        both:       true,  // test the primary and secondary modules.
+        both:       false,  // test the primary and secondary modules.
         ignoreError:false, // ignore error.
         callback:   function() {
         },
